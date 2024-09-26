2e024d5e6899d0d1dcf39deef4142561a5380a1f
Denidu Gamage <denidugamage21@gmail.com>
2024-09-26 10:23:55 +0530

    Initial Commit

---
+++ ShoppingCart/ShoppingCartApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingCartApp.swift
+//  ShoppingCart
+//
+//  Created by Denidu Gamage on 2024-09-26.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingCartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
